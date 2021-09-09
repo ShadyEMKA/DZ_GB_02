7551c40d5b1c17ab73b7bdd7f9e2b2cf173d133a
Andrei <payalnik17@gmail.com>
2021-09-09 12:58:17 +0300

    add L2_ShkundaliouAV

---
+++ CloneVK/CloneVK/Menu/Friends/FriendsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FriendsViewController.swift
+//  CloneVK
+//
+//  Created by Андрей Шкундалёв on 8.09.21.
+//
+
+import UIKit
+
+class FriendsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
