013e717cf5bdc8419295de57e83824d33401c613
Andrei <payalnik17@gmail.com>
2021-09-27 14:31:31 +0300

    add ShkundaliouAV

---
+++ VK/VK/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  VK
+//
+//  Created by Андрей Шкундалёв on 22.09.21.
+//
+
+import UIKit
+
+struct User {
+    let name: String
+    let avatar: UIImage?
+    let allPhoto: [UIImage?]
+}
